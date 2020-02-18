2898aa2d8e5ad9bcd5184e72999512febec5e9d2
Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>
2020-02-18 10:43:38 +0100

    Adapt to new integration tests layout

--- test-integration.sh
+++ test-integration.sh
@@ -2,4 +2,9 @@
 bz=$(realpath output/images/bzImage)
 mbd=$(realpath grub/output/target/boot/)
 cd $GOPATH/src/github.com/u-root/u-root/integration
-UROOT_QEMU_TIMEOUT_X=4 UROOT_QEMU="qemu-system-x86_64 -m 1024" UROOT_KERNEL="$bz" UROOT_MULTIBOOT_TEST_KERNEL_DIR="$mbd" go test "$@"
+export UROOT_QEMU_TIMEOUT_X=4
+export UROOT_QEMU="qemu-system-x86_64 -m 1024"
+export UROOT_KERNEL="$bz"
+export UROOT_MULTIBOOT_TEST_KERNEL_DIR="$mbd"
+
+go test "$@" ./...
